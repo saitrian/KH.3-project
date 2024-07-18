6eac282a5ff19033653613811afd17c10075ec31
suvin5027 <123538283+suvin5027@users.noreply.github.com>
2024-07-18 20:39:05 +0900

    [psv] utility 내용 저장
    [psv] utility 내용 저장

--- src/important/Utility.java
+++ src/important/Utility.java
@@ -5,12 +5,6 @@ import java.util.Scanner;
 public class Utility {
     private static Utility instance;
 	
-	public static final int TICKETING = 1;
-	public final int REFUND = 2;
-	public final int CHECK = 3;
-	public final int PRODUCE = 4;
-	public final int EXIT = 5;
-	
 	public Scanner scan = new Scanner(System.in);
 
     private Utility() {
