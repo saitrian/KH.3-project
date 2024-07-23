7030480c31165b73d33ebb4f19b2dd1578e6d101
saitrian <dhwodud200@gmail.com>
2024-07-23 19:40:16 +0900

    ㅍ

--- src/ticket/Ticket.java
+++ src/ticket/Ticket.java
@@ -5,13 +5,18 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
+// 무엇인지
 public class Ticket {
+	// 무엇인지
 	private String movieName;
+	// 얘는 무엇ㅇ니지
 	private String theater;
 	private String date;	
 	
 	@Override
+	// 무엇ㅇ니지
 	public String toString() {
+		// 얘가 행위가 
 		return "제목 : " + movieName + ", 상영관 : " + theater + ", 날짜 : " + date;
 	}
 }
