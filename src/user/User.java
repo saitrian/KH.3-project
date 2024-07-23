d6b8aa2e0f7c0a03ecd4cc307e9893c8bfbd79b4
saitrian <dhwodud200@gmail.com>
2024-07-23 12:41:24 +0900

    Update User.java

--- src/user/User.java
+++ src/user/User.java
@@ -3,13 +3,17 @@ package user;
 public class User {
 	private String name;
 	private String password;
-	
 	private int point;
 	
-	public User(String name, String pw) {
+	public User(String name, String pw, int point) {
 		this.name = name;
 		this.password = pw;
 		
 		this.point = 0;
+		
 	}
+	//user의 userlist의 객체가 늘어날때마다 point++
+	
+	//user가 refound로 영화 예매를 취소하면 point--
+			
 }
