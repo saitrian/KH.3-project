2c39643a0cea734cfab2622cd359e95b89657cbc
saitrian <dhwodud200@gmail.com>
2024-07-30 17:32:02 +0900

    [ojy] 수정

--- src/teamProject/ticket/Ticket.java
+++ src/teamProject/ticket/Ticket.java
@@ -14,6 +14,6 @@ public class Ticket {
 	@Override
 	public String toString() {
 		//원하는 출력 메세지를 만들기 위해 수정
-		return "영화 이름 : " + movieName + " / 장르 : " + genre + " / 날짜 : " + date + " / 상영관 " + theater + "관";
+		return "영화 이름 : " + movieName + " / 장르 : " + genre + " / 날짜 : " + date + " / 상영관 : " + theater + "관";
 	}
 }
