7edbf177dff3e00885fd594760c510b23db42711
suvin5027 <123538283+suvin5027@users.noreply.github.com>
2024-07-31 20:29:14 +0900

    [psv] schedule insert 수정 중
    [psv] schedule insert 수정 중

--- src/sjk/sample_2/teamProject/controller/ScheduleController.java
+++ src/sjk/sample_2/teamProject/controller/ScheduleController.java
@@ -2,9 +2,20 @@ package sjk.sample_2.teamProject.controller;
 
 import java.util.Scanner;
 
-public class ScheduleController extends BaseController {
+import sjk.sample_2.teamProject.model.vo.ScheduleVO;
+import sjk.sample_2.teamProject.service.ScheduleService;
+import sjk.sample_2.teamProject.service.ScheduleServiceImp;
 
+public class ScheduleController extends BaseController {
+	
+	private ScheduleService scheduleService = new ScheduleServiceImp();
+	
+	
 	public ScheduleController(Scanner scan) {
 		super(scan);
+	}
+	
+	public void insertSchedule() {
+		ScheduleVO schedule = new ScheduleVO("20:00", "2024-07-30", 1, 1);
 	}
 }
--- src/sjk/sample_2/teamProject/model/vo/ScheduleVO.java
+++ src/sjk/sample_2/teamProject/model/vo/ScheduleVO.java
@@ -1,5 +1,23 @@
 package sjk.sample_2.teamProject.model.vo;
 
-public class ScheduleVO {
+import java.util.Date;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ScheduleVO {
+	private int sd_num;
+	private Date sd_time; // 상영 시작 시간
+	private Date sd_date; // 상영 시작 날짜
+	private int sd_mo_num; 
+	private int sd_sc_num;
+
+	public ScheduleVO(Date sdTime, Date sdDate, int sdMoNum, int sdScNum) {
+		this.sd_time = sdTime;
+		this.sd_date = sdDate;
+		this.sd_mo_num = sdMoNum;
+		this.sd_sc_num = sdScNum;
+	}
 }
