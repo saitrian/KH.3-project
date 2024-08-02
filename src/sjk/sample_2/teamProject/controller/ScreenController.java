4529f27dd36c792fe16a6130801f92af7ae43203
saitrian <dhwodud200@gmail.com>
2024-08-02 13:59:31 +0900

    [ojy]수정

--- src/sjk/sample_2/teamProject/controller/ScreenController.java
+++ src/sjk/sample_2/teamProject/controller/ScreenController.java
@@ -28,11 +28,12 @@ public class ScreenController extends BaseController {
 		int sc_seat = 10;	//총 좌석 수
 		int sc_th_num = 1;	//극장 번호
 		List<ScreenVO> list = new ArrayList<ScreenVO>();
+		//i = 극장 번호, j = 상영관 이름 번호
 		for(int i = 1; i <= 5; i++) {
 			for(int j = 1; j <= 5; j++) {
-				sc_name = i + "관";
+				sc_name = j + "관";
 				sc_seat = 10;
-				sc_th_num = j;
+				sc_th_num = i;
 				ScreenVO sv = new ScreenVO(sc_name, sc_seat, sc_th_num);
 				list.add(sv);
 			}
